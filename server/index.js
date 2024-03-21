612c7b2b6cb9484844fab777a9eff3e9a370816e
himanshukrmr <himanshukrmr@gmail.com>
2024-03-21 16:14:58 +0530

    added url handling codes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
+    if(req.url == '/favicon.ico'){
+        return res.end();
+    }
   const log = `${Date.now()} ${req.url} New Req Received\n`;
+  const myUrl = url.parse(req.url, true);
+
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Welcome to the Home page");
         break;
       case "/about":
         res.end("Welcome to the About Page");
+        break;
+      case "/search":
+        const searchResults = myUrl.query.search_query
+        res.end(`These are your search results ${searchResults}`);
         break;
       default:
         res.end("Page not found");
