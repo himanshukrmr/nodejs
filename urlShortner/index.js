718e12f9238311f811bd64b0716036d2449774c1
himanshukrmr <himanshukrmr@gmail.com>
2024-04-11 15:56:07 +0530

    added views folder and ejs implementation

--- urlShortner/index.js
+++ urlShortner/index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const  urlRoutes = require("./routes/url")
+const staticRouter = require('./routes/staticRouter');
 const {connectToMongoDB} = require("./connect")
 const {URL} = require("./models/url")
+const path = require("path");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 const PORT = 8001;
 connectToMongoDB(`mongodb://127.0.0.1:27017/urlShortner`).then(() =>{
     console.log("MongoDB Connected")
 })
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+
 app.use("/url", urlRoutes)
+app.use('/', staticRouter)
+
+// app.get("/test", async(req,res) =>{
+//     const allUrls = await URL.find({});
+//     return res.render("home", {
+//         urls : allUrls,
+//     });
+// });
 
 app.get("/:shortId", async(req,res) =>{
     const shortId = req.params.shortId;
@@ -25,7 +41,12 @@ app.get("/:shortId", async(req,res) =>{
         }
     }
     )
-    res.redirect(entry.redirectUrl)
+    if (entry && entry.redirectUrl) {
+        res.redirect(entry.redirectUrl);
+    } else {
+        res.status(404).send("URL not found");
+    }
+    
 })
 
 app.listen(PORT,() =>{
